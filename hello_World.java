370675e63bfdf0143b4d65d86df0ddd7af4ef33a
BSimon1337 <91400559+BSimon1337@users.noreply.github.com>
2021-12-09 17:56:04 -0600

    Add files via upload

---
+++ hello_World.java
@@ -0,0 +1,44 @@
+package hello_Word;
+
+public class hello_World {
+
+	public static void main(String[] args) {
+		//single line comment
+		/*
+		 block comment
+		 asterisk do not matter
+		 always take notes
+		 */
+		
+		
+		//the stuff in quotes are called literals
+		//print statements
+		System.out.println("Hello, World");
+		
+		/*
+		 * numeric whole number types:
+		 * byte
+		 * short
+		 * int
+		 * long
+		 */
+		
+		int a; //declaring -->type name
+		a = 5; //initializing
+		a = 7;
+		a = 10;
+		
+		int b = 7; //single line declaration and initialization
+		
+		/*
+		 * precision variable
+		 * float
+		 * double
+		 */
+		
+		double c = 7.9;
+		
+		
+	}
+
+}
